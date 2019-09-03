537140b6fc407bf68ab0ffde67e62785154d4cf0
Gjert Ingar Gjersund <gjert@gjersund.com>
2019-09-03 15:04:21 +0200

    Update repo settings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import consoleLog from './test.js';
 import style from './static/css/spreadsheet.scss';
 
+consoleLog();
 console.log("Hello World from your main file!");
---
+++ src/test.js
@@ -0,0 +1,3 @@
+export default function consoleLog() {
+  console.log("Testing a module");
+}
